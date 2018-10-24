d1193f34f313138e9d8094a7e37ce9d455c63575
Laura Lopez <laura.lopez@mediatechsolutions.es>
2018-10-24 16:11:38 +0200

    Rename chooseFlightPage to flighPage

--- tests/step_definitions/payments_definitions.js
+++ tests/step_definitions/payments_definitions.js
@@ -11,13 +11,13 @@ var EC = protractor.ExpectedConditions;
 var timeout = 5000; // in miliseconds
 
 var mainPage = require('../pages/mainPage.js');
-var chooseFlightPage = require('../pages/chooseFlightPage.js');
+var flightPage = require('../pages/flightPage.js');
 
 Given(/^I make a booking from "([^"]*)" to "([^"]*)" on (\d+)\/(\d+)\/(\d+) for (\d+) adults and (\d+) child$/,
     function (departure, destination, day, month, year, adults, child) {
         mainPage.searchFlights(departure, destination, day, month, year, adults, child);
-        browser.wait(EC.visibilityOf(chooseFlightPage.firstFlight), timeout);
-        chooseFlightPage.chooseAnyFlight();
+        browser.wait(EC.visibilityOf(flightPage.firstFlight), timeout);
+        flightPage.chooseAnyFlight();
         return expect(browser.getCurrentUrl()).to.eventually.equal("");
 
 });
